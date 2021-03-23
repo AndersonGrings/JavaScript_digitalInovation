fa89930615fa857acf5b930476e7e6b832cb8fbc
Anderson Costa <anderson.em2010@hotmail.com>
2021-03-23 18:53:18 -0300

    fim de curso javaScript DigitalInovation

---
+++ public_javaScript/js/JS main.js
@@ -0,0 +1,73 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado"
+}
+
+function redirecionar(){
+    window.open("www.americanas.com");
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o rato"
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o rato"
+}
+
+
+/*var nome = "Anderson Costa";
+var idade = 28;
+var frase = "olha aquela nuvem";
+alert("Bem-vindo " + nome + " idade: " + idade + " anos");
+console.log(nome);
+console.log(idade);
+console.log (frase);*/
+
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista. length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+/*var fruta ={nome:"laranja", cor:"verde"}
+console.log(fruta.nome);
+alert(fruta.cor)*/
+
+/*var frutas =[{nome:"laranja", cor:"verde"}, {nome:"uva", cor:"azul"}]
+console.log(frutas);
+alert(frutas[1].nome)*/
+
+/*
+var idade = prompt("qual sua idade?");
+if (idade>= 18 && idade<65){
+    alert("maior de idade");
+} else if (idade>= 65){
+    alert("idoso");
+}
+else{
+    alert("menor de idade");
+};*/
+
+/*var count = 0;
+while ( count <= 5){
+    console.log(count);
+    alert (count);
+    count++;
+};*/
+
+/*var count;
+for ( count = 0; count <= 5; count++)
+    alert (count);*/
+
+   /* function soma ( n1, n2){
+        return n1+n2;
+    }
+    alert (soma(5,10));*/
+
+
+
+
+
+
